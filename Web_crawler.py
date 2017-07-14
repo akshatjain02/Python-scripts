3f29da30a343305f06baf8646f19b5d72102b07e
Akshat Jain <akshatjain0205@yahoo.co.in>
2017-07-15 03:20:07 +0530

    Add files via upload

--- Web_crawler.py
+++ Web_crawler.py
@@ -13,8 +13,20 @@ def crawler(max_pages):
         for link in soup.findAll('a', {'class':'title text-semibold'}):
             href = link.get('href')
             title = link.string
-            print(href)
+            #print(href)
             print(title)
+            get_info_inside(href)
         pages += 1
+
+
+def get_info_inside(url):
+    source_code = requests.get(url)
+    plain_text = source_code.text
+    soup = BeautifulSoup(plain_text, "html.parser")
+    for link in soup.findAll('span', {'class':'date'}):
+        print(link.string)
+
+
+
 
 crawler(1)
